cf2889e1024c75b01b73eaa85cde09c8ad7b6e20
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2019-09-01 14:34:59 -0700

    basic timer functionality
    also put everything in boba tea namespace

--- Boba Tea/Assets/Editor/DayCycleSettingsEditor.cs
+++ Boba Tea/Assets/Editor/DayCycleSettingsEditor.cs
@@ -6,229 +6,233 @@ using UnityEditor.Rendering.PostProcessing;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
-[PostProcessEditor(typeof(DayCycleSettings))]
-public class DayCycleSettingsEditor : PostProcessEffectEditor<DayCycleSettings>
-{
-    SerializedParameterOverride m_time;
-    SerializedParameterOverride m_minTime;
-    SerializedParameterOverride m_maxTime;
+namespace BobaTea {
 
-    SerializedParameterOverride m_key0;
-    SerializedParameterOverride m_key1;
-    SerializedParameterOverride m_key2;
-    SerializedParameterOverride m_key3;
-    SerializedParameterOverride m_key4;
-    SerializedParameterOverride m_key5;
-    SerializedParameterOverride m_key6;
-    SerializedParameterOverride m_key7;
-
-    SerializedParameterOverride m_time0;
-    SerializedParameterOverride m_time1;
-    SerializedParameterOverride m_time2;
-    SerializedParameterOverride m_time3;
-    SerializedParameterOverride m_time4;
-    SerializedParameterOverride m_time5;
-    SerializedParameterOverride m_time6;
-    SerializedParameterOverride m_time7;
-
-    SerializedParameterOverride m_time0s;
-    SerializedParameterOverride m_time1s;
-    SerializedParameterOverride m_time2s;
-    SerializedParameterOverride m_time3s;
-    SerializedParameterOverride m_time4s;
-    SerializedParameterOverride m_time5s;
-    SerializedParameterOverride m_time6s;
-    SerializedParameterOverride m_time7s;
-
-    Gradient gradient;
-
-    public override void OnEnable () 
+    [PostProcessEditor(typeof(DayCycleSettings))]
+    public class DayCycleSettingsEditor : PostProcessEffectEditor<DayCycleSettings>
     {
-        m_time = FindParameterOverride(x => x.time);
-        m_minTime = FindParameterOverride(x => x.minTime);
-        m_maxTime = FindParameterOverride(x => x.maxTime);
+        SerializedParameterOverride m_time;
+        SerializedParameterOverride m_minTime;
+        SerializedParameterOverride m_maxTime;
 
-        m_key0 = FindParameterOverride(x => x.key0);
-        m_key1 = FindParameterOverride(x => x.key1);
-        m_key2 = FindParameterOverride(x => x.key2);
-        m_key3 = FindParameterOverride(x => x.key3);
-        m_key4 = FindParameterOverride(x => x.key4);
-        m_key5 = FindParameterOverride(x => x.key5);
-        m_key6 = FindParameterOverride(x => x.key6);
-        m_key7 = FindParameterOverride(x => x.key7);
+        SerializedParameterOverride m_key0;
+        SerializedParameterOverride m_key1;
+        SerializedParameterOverride m_key2;
+        SerializedParameterOverride m_key3;
+        SerializedParameterOverride m_key4;
+        SerializedParameterOverride m_key5;
+        SerializedParameterOverride m_key6;
+        SerializedParameterOverride m_key7;
 
-        m_time0 = FindParameterOverride(x => x.time0);
-        m_time1 = FindParameterOverride(x => x.time1);
-        m_time2 = FindParameterOverride(x => x.time2);
-        m_time3 = FindParameterOverride(x => x.time3);
-        m_time4 = FindParameterOverride(x => x.time4);
-        m_time5 = FindParameterOverride(x => x.time5);
-        m_time6 = FindParameterOverride(x => x.time6);
-        m_time7 = FindParameterOverride(x => x.time7);
+        SerializedParameterOverride m_time0;
+        SerializedParameterOverride m_time1;
+        SerializedParameterOverride m_time2;
+        SerializedParameterOverride m_time3;
+        SerializedParameterOverride m_time4;
+        SerializedParameterOverride m_time5;
+        SerializedParameterOverride m_time6;
+        SerializedParameterOverride m_time7;
 
-        m_time0s = FindParameterOverride(x => x.time0s);
-        m_time1s = FindParameterOverride(x => x.time1s);
-        m_time2s = FindParameterOverride(x => x.time2s);
-        m_time3s = FindParameterOverride(x => x.time3s);
-        m_time4s = FindParameterOverride(x => x.time4s);
-        m_time5s = FindParameterOverride(x => x.time5s);
-        m_time6s = FindParameterOverride(x => x.time6s);
-        m_time7s = FindParameterOverride(x => x.time7s);
+        SerializedParameterOverride m_time0s;
+        SerializedParameterOverride m_time1s;
+        SerializedParameterOverride m_time2s;
+        SerializedParameterOverride m_time3s;
+        SerializedParameterOverride m_time4s;
+        SerializedParameterOverride m_time5s;
+        SerializedParameterOverride m_time6s;
+        SerializedParameterOverride m_time7s;
 
-        gradient = new Gradient();
+        Gradient gradient;
 
-        GradientColorKey[] colorKeys = new GradientColorKey[] {
-            new GradientColorKey(m_key0.value.colorValue, m_time0.value.floatValue),
-            new GradientColorKey(m_key1.value.colorValue, m_time1.value.floatValue),
-            new GradientColorKey(m_key2.value.colorValue, m_time2.value.floatValue),
-            new GradientColorKey(m_key3.value.colorValue, m_time3.value.floatValue),
-            new GradientColorKey(m_key4.value.colorValue, m_time4.value.floatValue),
-            new GradientColorKey(m_key5.value.colorValue, m_time5.value.floatValue),
-            new GradientColorKey(m_key6.value.colorValue, m_time6.value.floatValue),
-            new GradientColorKey(m_key7.value.colorValue, m_time7.value.floatValue)
-        };
+        public override void OnEnable () 
+        {
+            m_time = FindParameterOverride(x => x.time);
+            m_minTime = FindParameterOverride(x => x.minTime);
+            m_maxTime = FindParameterOverride(x => x.maxTime);
 
-        GradientAlphaKey[] alphaKeys = new GradientAlphaKey [] {
-            new GradientAlphaKey(m_key0.value.colorValue.a, m_time0s.value.floatValue),
-            new GradientAlphaKey(m_key1.value.colorValue.a, m_time1s.value.floatValue),
-            new GradientAlphaKey(m_key2.value.colorValue.a, m_time2s.value.floatValue),
-            new GradientAlphaKey(m_key3.value.colorValue.a, m_time3s.value.floatValue),
-            new GradientAlphaKey(m_key4.value.colorValue.a, m_time4s.value.floatValue),
-            new GradientAlphaKey(m_key5.value.colorValue.a, m_time5s.value.floatValue),
-            new GradientAlphaKey(m_key6.value.colorValue.a, m_time6s.value.floatValue),
-            new GradientAlphaKey(m_key7.value.colorValue.a, m_time7s.value.floatValue)
-        };
+            m_key0 = FindParameterOverride(x => x.key0);
+            m_key1 = FindParameterOverride(x => x.key1);
+            m_key2 = FindParameterOverride(x => x.key2);
+            m_key3 = FindParameterOverride(x => x.key3);
+            m_key4 = FindParameterOverride(x => x.key4);
+            m_key5 = FindParameterOverride(x => x.key5);
+            m_key6 = FindParameterOverride(x => x.key6);
+            m_key7 = FindParameterOverride(x => x.key7);
 
-        gradient.SetKeys(colorKeys, alphaKeys);
-    }
+            m_time0 = FindParameterOverride(x => x.time0);
+            m_time1 = FindParameterOverride(x => x.time1);
+            m_time2 = FindParameterOverride(x => x.time2);
+            m_time3 = FindParameterOverride(x => x.time3);
+            m_time4 = FindParameterOverride(x => x.time4);
+            m_time5 = FindParameterOverride(x => x.time5);
+            m_time6 = FindParameterOverride(x => x.time6);
+            m_time7 = FindParameterOverride(x => x.time7);
 
-    public override void OnInspectorGUI () {
-        PropertyField(m_time);
-        PropertyField(m_minTime);
-        PropertyField(m_maxTime);
+            m_time0s = FindParameterOverride(x => x.time0s);
+            m_time1s = FindParameterOverride(x => x.time1s);
+            m_time2s = FindParameterOverride(x => x.time2s);
+            m_time3s = FindParameterOverride(x => x.time3s);
+            m_time4s = FindParameterOverride(x => x.time4s);
+            m_time5s = FindParameterOverride(x => x.time5s);
+            m_time6s = FindParameterOverride(x => x.time6s);
+            m_time7s = FindParameterOverride(x => x.time7s);
 
-        gradient = EditorGUILayout.GradientField("Color and Saturation Gradient", gradient);
-        SetGradientValues();
-    }
+            gradient = new Gradient();
 
-    private void SetGradientValues () {
-        GradientColorKey[] colorKeys = new GradientColorKey[8];
-        for (int i = 0; i < 8; i++) {
-            if(i < gradient.colorKeys.Length) {
-                colorKeys[i] = gradient.colorKeys[i];
-            } else {
-                colorKeys[i] = new GradientColorKey(Color.black, -1);
-            }
+            GradientColorKey[] colorKeys = new GradientColorKey[] {
+                new GradientColorKey(m_key0.value.colorValue, m_time0.value.floatValue),
+                new GradientColorKey(m_key1.value.colorValue, m_time1.value.floatValue),
+                new GradientColorKey(m_key2.value.colorValue, m_time2.value.floatValue),
+                new GradientColorKey(m_key3.value.colorValue, m_time3.value.floatValue),
+                new GradientColorKey(m_key4.value.colorValue, m_time4.value.floatValue),
+                new GradientColorKey(m_key5.value.colorValue, m_time5.value.floatValue),
+                new GradientColorKey(m_key6.value.colorValue, m_time6.value.floatValue),
+                new GradientColorKey(m_key7.value.colorValue, m_time7.value.floatValue)
+            };
+
+            GradientAlphaKey[] alphaKeys = new GradientAlphaKey [] {
+                new GradientAlphaKey(m_key0.value.colorValue.a, m_time0s.value.floatValue),
+                new GradientAlphaKey(m_key1.value.colorValue.a, m_time1s.value.floatValue),
+                new GradientAlphaKey(m_key2.value.colorValue.a, m_time2s.value.floatValue),
+                new GradientAlphaKey(m_key3.value.colorValue.a, m_time3s.value.floatValue),
+                new GradientAlphaKey(m_key4.value.colorValue.a, m_time4s.value.floatValue),
+                new GradientAlphaKey(m_key5.value.colorValue.a, m_time5s.value.floatValue),
+                new GradientAlphaKey(m_key6.value.colorValue.a, m_time6s.value.floatValue),
+                new GradientAlphaKey(m_key7.value.colorValue.a, m_time7s.value.floatValue)
+            };
+
+            gradient.SetKeys(colorKeys, alphaKeys);
         }
 
-        GradientAlphaKey[] alphaKeys = new GradientAlphaKey[8];
-        for(int i = 0; i < 8; i++) {
-            if(i < gradient.alphaKeys.Length) {
-                alphaKeys[i] = gradient.alphaKeys[i];
-            } else {
-                alphaKeys[i] = new GradientAlphaKey(0, -1);
-            }
+        public override void OnInspectorGUI () {
+            PropertyField(m_time);
+            PropertyField(m_minTime);
+            PropertyField(m_maxTime);
+
+            gradient = EditorGUILayout.GradientField("Color and Saturation Gradient", gradient);
+            SetGradientValues();
         }
 
-        m_key0.overrideState.boolValue = true;
-        m_key1.overrideState.boolValue = true;
-        m_key2.overrideState.boolValue = true;
-        m_key3.overrideState.boolValue = true;
-        m_key4.overrideState.boolValue = true;
-        m_key5.overrideState.boolValue = true;
-        m_key6.overrideState.boolValue = true;
-        m_key7.overrideState.boolValue = true;
+        private void SetGradientValues () {
+            GradientColorKey[] colorKeys = new GradientColorKey[8];
+            for (int i = 0; i < 8; i++) {
+                if(i < gradient.colorKeys.Length) {
+                    colorKeys[i] = gradient.colorKeys[i];
+                } else {
+                    colorKeys[i] = new GradientColorKey(Color.black, -1);
+                }
+            }
 
-        m_time0.overrideState.boolValue = true;
-        m_time1.overrideState.boolValue = true;
-        m_time2.overrideState.boolValue = true;
-        m_time3.overrideState.boolValue = true;
-        m_time4.overrideState.boolValue = true;
-        m_time5.overrideState.boolValue = true;
-        m_time6.overrideState.boolValue = true;
-        m_time7.overrideState.boolValue = true;
+            GradientAlphaKey[] alphaKeys = new GradientAlphaKey[8];
+            for(int i = 0; i < 8; i++) {
+                if(i < gradient.alphaKeys.Length) {
+                    alphaKeys[i] = gradient.alphaKeys[i];
+                } else {
+                    alphaKeys[i] = new GradientAlphaKey(0, -1);
+                }
+            }
 
-        m_time0s.overrideState.boolValue = true;
-        m_time1s.overrideState.boolValue = true;
-        m_time2s.overrideState.boolValue = true;
-        m_time3s.overrideState.boolValue = true;
-        m_time4s.overrideState.boolValue = true;
-        m_time5s.overrideState.boolValue = true;
-        m_time6s.overrideState.boolValue = true;
-        m_time7s.overrideState.boolValue = true;
+            m_key0.overrideState.boolValue = true;
+            m_key1.overrideState.boolValue = true;
+            m_key2.overrideState.boolValue = true;
+            m_key3.overrideState.boolValue = true;
+            m_key4.overrideState.boolValue = true;
+            m_key5.overrideState.boolValue = true;
+            m_key6.overrideState.boolValue = true;
+            m_key7.overrideState.boolValue = true;
 
-        m_key0.value.colorValue = new Color(
-           colorKeys[0].color.r,
-           colorKeys[0].color.g,
-           colorKeys[0].color.b,
-           alphaKeys[0].alpha
-        );
+            m_time0.overrideState.boolValue = true;
+            m_time1.overrideState.boolValue = true;
+            m_time2.overrideState.boolValue = true;
+            m_time3.overrideState.boolValue = true;
+            m_time4.overrideState.boolValue = true;
+            m_time5.overrideState.boolValue = true;
+            m_time6.overrideState.boolValue = true;
+            m_time7.overrideState.boolValue = true;
 
-        m_key1.value.colorValue = new Color(
-           colorKeys[1].color.r,
-           colorKeys[1].color.g,
-           colorKeys[1].color.b,
-           alphaKeys[1].alpha
-        );
+            m_time0s.overrideState.boolValue = true;
+            m_time1s.overrideState.boolValue = true;
+            m_time2s.overrideState.boolValue = true;
+            m_time3s.overrideState.boolValue = true;
+            m_time4s.overrideState.boolValue = true;
+            m_time5s.overrideState.boolValue = true;
+            m_time6s.overrideState.boolValue = true;
+            m_time7s.overrideState.boolValue = true;
 
-        m_key2.value.colorValue = new Color(
-           colorKeys[2].color.r,
-           colorKeys[2].color.g,
-           colorKeys[2].color.b,
-           alphaKeys[2].alpha
-        );
+            m_key0.value.colorValue = new Color(
+            colorKeys[0].color.r,
+            colorKeys[0].color.g,
+            colorKeys[0].color.b,
+            alphaKeys[0].alpha
+            );
 
-        m_key3.value.colorValue = new Color(
-           colorKeys[3].color.r,
-           colorKeys[3].color.g,
-           colorKeys[3].color.b,
-           alphaKeys[3].alpha
-        );
+            m_key1.value.colorValue = new Color(
+            colorKeys[1].color.r,
+            colorKeys[1].color.g,
+            colorKeys[1].color.b,
+            alphaKeys[1].alpha
+            );
 
-        m_key4.value.colorValue = new Color(
-           colorKeys[4].color.r,
-           colorKeys[4].color.g,
-           colorKeys[4].color.b,
-           alphaKeys[4].alpha
-        );
+            m_key2.value.colorValue = new Color(
+            colorKeys[2].color.r,
+            colorKeys[2].color.g,
+            colorKeys[2].color.b,
+            alphaKeys[2].alpha
+            );
 
-        m_key5.value.colorValue = new Color(
-           colorKeys[5].color.r,
-           colorKeys[5].color.g,
-           colorKeys[5].color.b,
-           alphaKeys[5].alpha
-        );
+            m_key3.value.colorValue = new Color(
+            colorKeys[3].color.r,
+            colorKeys[3].color.g,
+            colorKeys[3].color.b,
+            alphaKeys[3].alpha
+            );
 
-        m_key6.value.colorValue = new Color(
-           colorKeys[6].color.r,
-           colorKeys[6].color.g,
-           colorKeys[6].color.b,
-           alphaKeys[6].alpha
-        );
+            m_key4.value.colorValue = new Color(
+            colorKeys[4].color.r,
+            colorKeys[4].color.g,
+            colorKeys[4].color.b,
+            alphaKeys[4].alpha
+            );
 
-        m_key7.value.colorValue = new Color(
-           colorKeys[7].color.r,
-           colorKeys[7].color.g,
-           colorKeys[7].color.b,
-           alphaKeys[7].alpha
-        );
+            m_key5.value.colorValue = new Color(
+            colorKeys[5].color.r,
+            colorKeys[5].color.g,
+            colorKeys[5].color.b,
+            alphaKeys[5].alpha
+            );
 
-        m_time0.value.floatValue = colorKeys[0].time;
-        m_time1.value.floatValue = colorKeys[1].time;
-        m_time2.value.floatValue = colorKeys[2].time;
-        m_time3.value.floatValue = colorKeys[3].time;
-        m_time4.value.floatValue = colorKeys[4].time;
-        m_time5.value.floatValue = colorKeys[5].time;
-        m_time6.value.floatValue = colorKeys[6].time;
-        m_time7.value.floatValue = colorKeys[7].time;
+            m_key6.value.colorValue = new Color(
+            colorKeys[6].color.r,
+            colorKeys[6].color.g,
+            colorKeys[6].color.b,
+            alphaKeys[6].alpha
+            );
 
-        m_time0s.value.floatValue = alphaKeys[0].time;
-        m_time1s.value.floatValue = alphaKeys[1].time;
-        m_time2s.value.floatValue = alphaKeys[2].time;
-        m_time3s.value.floatValue = alphaKeys[3].time;
-        m_time4s.value.floatValue = alphaKeys[4].time;
-        m_time5s.value.floatValue = alphaKeys[5].time;
-        m_time6s.value.floatValue = alphaKeys[6].time;
-        m_time7s.value.floatValue = alphaKeys[7].time;
+            m_key7.value.colorValue = new Color(
+            colorKeys[7].color.r,
+            colorKeys[7].color.g,
+            colorKeys[7].color.b,
+            alphaKeys[7].alpha
+            );
+
+            m_time0.value.floatValue = colorKeys[0].time;
+            m_time1.value.floatValue = colorKeys[1].time;
+            m_time2.value.floatValue = colorKeys[2].time;
+            m_time3.value.floatValue = colorKeys[3].time;
+            m_time4.value.floatValue = colorKeys[4].time;
+            m_time5.value.floatValue = colorKeys[5].time;
+            m_time6.value.floatValue = colorKeys[6].time;
+            m_time7.value.floatValue = colorKeys[7].time;
+
+            m_time0s.value.floatValue = alphaKeys[0].time;
+            m_time1s.value.floatValue = alphaKeys[1].time;
+            m_time2s.value.floatValue = alphaKeys[2].time;
+            m_time3s.value.floatValue = alphaKeys[3].time;
+            m_time4s.value.floatValue = alphaKeys[4].time;
+            m_time5s.value.floatValue = alphaKeys[5].time;
+            m_time6s.value.floatValue = alphaKeys[6].time;
+            m_time7s.value.floatValue = alphaKeys[7].time;
+        }
     }
+
 }
--- Boba Tea/Assets/Scripts/Rendering/DayCycleRenderer.cs
+++ Boba Tea/Assets/Scripts/Rendering/DayCycleRenderer.cs
@@ -2,79 +2,83 @@
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
-[Serializable]
-[PostProcess(typeof(DayCycleRenderer), PostProcessEvent.AfterStack, "Custom/DayNightCycle")]
-public sealed class DayCycleSettings : PostProcessEffectSettings
-{
-    [Range(0f, 24f), Tooltip("Normalized time of day.")]
-    public FloatParameter time = new FloatParameter {value = 12.0f};
-    public FloatParameter minTime = new FloatParameter {value = 0.0f};
-    public FloatParameter maxTime = new FloatParameter {value = 24.0f};
+namespace BobaTea {
 
-    public ColorParameter key0 = new ColorParameter {value = Color.white};
-    public ColorParameter key1 = new ColorParameter {value = Color.white};
-    public ColorParameter key2 = new ColorParameter {value = Color.white};
-    public ColorParameter key3 = new ColorParameter {value = Color.white};
-    public ColorParameter key4 = new ColorParameter {value = Color.white};
-    public ColorParameter key5 = new ColorParameter {value = Color.white};
-    public ColorParameter key6 = new ColorParameter {value = Color.white};
-    public ColorParameter key7 = new ColorParameter {value = Color.white};
+    [Serializable]
+    [PostProcess(typeof(DayCycleRenderer), PostProcessEvent.AfterStack, "Custom/DayNightCycle")]
+    public sealed class DayCycleSettings : PostProcessEffectSettings
+    {
+        [Range(0f, 24f), Tooltip("Normalized time of day.")]
+        public FloatParameter time = new FloatParameter {value = 12.0f};
+        public FloatParameter minTime = new FloatParameter {value = 0.0f};
+        public FloatParameter maxTime = new FloatParameter {value = 24.0f};
 
-    public FloatParameter time0 = new FloatParameter {value = 0.0f};
-    public FloatParameter time1 = new FloatParameter {value = 0.0f};
-    public FloatParameter time2 = new FloatParameter {value = 0.0f};
-    public FloatParameter time3 = new FloatParameter {value = 0.0f};
-    public FloatParameter time4 = new FloatParameter {value = 0.0f};
-    public FloatParameter time5 = new FloatParameter {value = 0.0f};
-    public FloatParameter time6 = new FloatParameter {value = 0.0f};
-    public FloatParameter time7 = new FloatParameter {value = 0.0f};
+        public ColorParameter key0 = new ColorParameter {value = Color.white};
+        public ColorParameter key1 = new ColorParameter {value = Color.white};
+        public ColorParameter key2 = new ColorParameter {value = Color.white};
+        public ColorParameter key3 = new ColorParameter {value = Color.white};
+        public ColorParameter key4 = new ColorParameter {value = Color.white};
+        public ColorParameter key5 = new ColorParameter {value = Color.white};
+        public ColorParameter key6 = new ColorParameter {value = Color.white};
+        public ColorParameter key7 = new ColorParameter {value = Color.white};
 
-    public FloatParameter time0s = new FloatParameter {value = 0.0f};
-    public FloatParameter time1s = new FloatParameter {value = 0.0f};
-    public FloatParameter time2s = new FloatParameter {value = 0.0f};
-    public FloatParameter time3s = new FloatParameter {value = 0.0f};
-    public FloatParameter time4s = new FloatParameter {value = 0.0f};
-    public FloatParameter time5s = new FloatParameter {value = 0.0f};
-    public FloatParameter time6s = new FloatParameter {value = 0.0f};
-    public FloatParameter time7s = new FloatParameter {value = 0.0f};
-}
+        public FloatParameter time0 = new FloatParameter {value = 0.0f};
+        public FloatParameter time1 = new FloatParameter {value = 0.0f};
+        public FloatParameter time2 = new FloatParameter {value = 0.0f};
+        public FloatParameter time3 = new FloatParameter {value = 0.0f};
+        public FloatParameter time4 = new FloatParameter {value = 0.0f};
+        public FloatParameter time5 = new FloatParameter {value = 0.0f};
+        public FloatParameter time6 = new FloatParameter {value = 0.0f};
+        public FloatParameter time7 = new FloatParameter {value = 0.0f};
 
-public class DayCycleRenderer : PostProcessEffectRenderer<DayCycleSettings>
-{
-    public override void Render(PostProcessRenderContext context) {
-        var sheet = context.propertySheets.Get(Shader.Find("Custom/DayNightCycle"));
-
-        sheet.properties.SetFloat("_DayTime", settings.time);
-        sheet.properties.SetFloat("_MaxTime", settings.maxTime);
-        sheet.properties.SetFloat("_MinTime", settings.minTime);
-
-        sheet.properties.SetFloat("_Time0", settings.time0);
-        sheet.properties.SetFloat("_Time1", settings.time1);
-        sheet.properties.SetFloat("_Time2", settings.time2);
-        sheet.properties.SetFloat("_Time3", settings.time3);
-        sheet.properties.SetFloat("_Time4", settings.time4);
-        sheet.properties.SetFloat("_Time5", settings.time5);
-        sheet.properties.SetFloat("_Time6", settings.time6);
-        sheet.properties.SetFloat("_Time7", settings.time7);
-
-        sheet.properties.SetFloat("_Time0s", settings.time0s);
-        sheet.properties.SetFloat("_Time1s", settings.time1s);
-        sheet.properties.SetFloat("_Time2s", settings.time2s);
-        sheet.properties.SetFloat("_Time3s", settings.time3s);
-        sheet.properties.SetFloat("_Time4s", settings.time4s);
-        sheet.properties.SetFloat("_Time5s", settings.time5s);
-        sheet.properties.SetFloat("_Time6s", settings.time6s);
-        sheet.properties.SetFloat("_Time7s", settings.time7s);
-
-        sheet.properties.SetColor("_Key0", settings.key0);
-        sheet.properties.SetColor("_Key1", settings.key1);
-        sheet.properties.SetColor("_Key2", settings.key2);
-        sheet.properties.SetColor("_Key3", settings.key3);
-        sheet.properties.SetColor("_Key4", settings.key4);
-        sheet.properties.SetColor("_Key5", settings.key5);
-        sheet.properties.SetColor("_Key6", settings.key6);
-        sheet.properties.SetColor("_Key7", settings.key7);
-        
-        context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+        public FloatParameter time0s = new FloatParameter {value = 0.0f};
+        public FloatParameter time1s = new FloatParameter {value = 0.0f};
+        public FloatParameter time2s = new FloatParameter {value = 0.0f};
+        public FloatParameter time3s = new FloatParameter {value = 0.0f};
+        public FloatParameter time4s = new FloatParameter {value = 0.0f};
+        public FloatParameter time5s = new FloatParameter {value = 0.0f};
+        public FloatParameter time6s = new FloatParameter {value = 0.0f};
+        public FloatParameter time7s = new FloatParameter {value = 0.0f};
     }
+
+    public class DayCycleRenderer : PostProcessEffectRenderer<DayCycleSettings>
+    {
+        public override void Render(PostProcessRenderContext context) {
+            var sheet = context.propertySheets.Get(Shader.Find("Custom/DayNightCycle"));
+
+            sheet.properties.SetFloat("_DayTime", settings.time);
+            sheet.properties.SetFloat("_MaxTime", settings.maxTime);
+            sheet.properties.SetFloat("_MinTime", settings.minTime);
+
+            sheet.properties.SetFloat("_Time0", settings.time0);
+            sheet.properties.SetFloat("_Time1", settings.time1);
+            sheet.properties.SetFloat("_Time2", settings.time2);
+            sheet.properties.SetFloat("_Time3", settings.time3);
+            sheet.properties.SetFloat("_Time4", settings.time4);
+            sheet.properties.SetFloat("_Time5", settings.time5);
+            sheet.properties.SetFloat("_Time6", settings.time6);
+            sheet.properties.SetFloat("_Time7", settings.time7);
+
+            sheet.properties.SetFloat("_Time0s", settings.time0s);
+            sheet.properties.SetFloat("_Time1s", settings.time1s);
+            sheet.properties.SetFloat("_Time2s", settings.time2s);
+            sheet.properties.SetFloat("_Time3s", settings.time3s);
+            sheet.properties.SetFloat("_Time4s", settings.time4s);
+            sheet.properties.SetFloat("_Time5s", settings.time5s);
+            sheet.properties.SetFloat("_Time6s", settings.time6s);
+            sheet.properties.SetFloat("_Time7s", settings.time7s);
+
+            sheet.properties.SetColor("_Key0", settings.key0);
+            sheet.properties.SetColor("_Key1", settings.key1);
+            sheet.properties.SetColor("_Key2", settings.key2);
+            sheet.properties.SetColor("_Key3", settings.key3);
+            sheet.properties.SetColor("_Key4", settings.key4);
+            sheet.properties.SetColor("_Key5", settings.key5);
+            sheet.properties.SetColor("_Key6", settings.key6);
+            sheet.properties.SetColor("_Key7", settings.key7);
+            
+            context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+        }
+    }
+
 }
